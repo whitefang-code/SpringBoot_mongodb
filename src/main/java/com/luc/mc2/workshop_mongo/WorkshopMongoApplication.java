ef7c3073e72830ad89786b0592046cce58c84cd5
whitefang-code <lucianoarn5@gmail.com>
2023-01-21 02:05:36 -0300

    First commit

---
+++ src/main/java/com/luc/mc2/workshop_mongo/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.luc.mc2.workshop_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luc/mc2/workshop_mongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luc.mc2.workshop_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
