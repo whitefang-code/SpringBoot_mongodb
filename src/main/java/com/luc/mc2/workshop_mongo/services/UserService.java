1ab35b8a79e93798d4564dda5e64f9cb9072a198
whitefang-code <lucianoarn5@gmail.com>
2023-01-22 16:49:25 -0300

    Getting user by ID.

--- src/main/java/com/luc/mc2/workshop_mongo/services/UserService.java
+++ src/main/java/com/luc/mc2/workshop_mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luc.mc2.workshop_mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luc.mc2.workshop_mongo.domain.User;
 import com.luc.mc2.workshop_mongo.repository.UserRepository;
+import com.luc.mc2.workshop_mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+
 
 }
---
+++ src/main/java/com/luc/mc2/workshop_mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.luc.mc2.workshop_mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
