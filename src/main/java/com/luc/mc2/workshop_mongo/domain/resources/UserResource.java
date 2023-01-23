c05d508809c318ba4991731bf2791c00158ee047
whitefang-code <lucianoarn5@gmail.com>
2023-01-23 06:46:35 -0300

    Update with PUT

--- src/main/java/com/luc/mc2/workshop_mongo/domain/resources/UserResource.java
+++ src/main/java/com/luc/mc2/workshop_mongo/domain/resources/UserResource.java
@@ -54,6 +54,15 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity <Void> Update(@RequestBody UserDTO objDto,@PathVariable String id) {
+		User  obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
 
 	
 	
--- src/main/java/com/luc/mc2/workshop_mongo/repository/UserRepository.java
+++ src/main/java/com/luc/mc2/workshop_mongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.luc.mc2.workshop_mongo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.luc.mc2.workshop_mongo.domain.User;
 
 @Repository
 public interface UserRepository  extends MongoRepository<User, String>{
+
+	User save(Optional<User> newObj);
 
 }
