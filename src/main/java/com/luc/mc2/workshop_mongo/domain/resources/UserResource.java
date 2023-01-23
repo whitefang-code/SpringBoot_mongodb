93291bf3724c4f6a73f71a18907faccca335fc17
whitefang-code <lucianoarn5@gmail.com>
2023-01-23 10:59:01 -0300

    End point to return user posts.

--- src/main/java/com/luc/mc2/workshop_mongo/domain/resources/UserResource.java
+++ src/main/java/com/luc/mc2/workshop_mongo/domain/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luc.mc2.workshop_mongo.domain.Post;
 import com.luc.mc2.workshop_mongo.domain.User;
 import com.luc.mc2.workshop_mongo.dto.UserDTO;
 import com.luc.mc2.workshop_mongo.services.UserService;
@@ -62,6 +63,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(value="/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id) {
+		User  obj = service.findById(id);
+		return ResponseEntity.ok().body( obj.getPosts());
+	}
+	
 	
 
 	
