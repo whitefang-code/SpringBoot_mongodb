163efe0af5a5db32e24efd06b4a5880ceeff0135
whitefang-code <lucianoarn5@gmail.com>
2023-01-24 10:04:33 -0300

    Consultation with various criteria

--- src/main/java/com/luc/mc2/workshop_mongo/domain/resources/PostResource.java
+++ src/main/java/com/luc/mc2/workshop_mongo/domain/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.luc.mc2.workshop_mongo.domain.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +39,18 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	
+	@RequestMapping(value="/fullsearch",method = RequestMethod.GET)
+	public ResponseEntity <List<Post>> fullSearch
+	(@RequestParam(value="text",defaultValue="")String text,
+	 @RequestParam(value="minDate",defaultValue="")String minDate,
+	 @RequestParam(value="maxDate",defaultValue="")String maxDate){
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date(0L));
+		List<Post> list = service.fullSearch(text,min,max);
+		return ResponseEntity.ok().body(list);
+	}
 
 
 }
--- src/main/java/com/luc/mc2/workshop_mongo/services/PostService.java
+++ src/main/java/com/luc/mc2/workshop_mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.luc.mc2.workshop_mongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	public List<Post> fullSearch(String text,Date minDate,Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 *60 * 60 *1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 
 }
