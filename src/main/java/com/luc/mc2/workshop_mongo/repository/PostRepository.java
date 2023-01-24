8a59250388e95de49a0e42a45e184cda058a8d00
whitefang-code <lucianoarn5@gmail.com>
2023-01-24 00:51:39 -0300

    Simple search with @query

--- src/main/java/com/luc/mc2/workshop_mongo/repository/PostRepository.java
+++ src/main/java/com/luc/mc2/workshop_mongo/repository/PostRepository.java
@@ -3,13 +3,17 @@ package com.luc.mc2.workshop_mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.luc.mc2.workshop_mongo.domain.Post;
 
 @Repository
 public interface PostRepository  extends MongoRepository<Post, String>{
-
+	
+	@Query("{'title':{$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
